212a51eb9e5ce1fa3b83b3247b3d60d89c6710aa
serene <sereneding123@gmail.com>
2021-06-07 11:43:12 +0800

    complete first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        
+        int sum = 0;
+        for (int i = 0; i < 10; i = i + 1) {
+        	sum = sum + i;
+            System.out.println(sum);
+            
+        }
+    }
+}
